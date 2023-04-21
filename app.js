33aa2f9b44793d6ae52b92deb0e8f7d925e6e0c5
Ut79 <94139591+Ut79@users.noreply.github.com>
2023-04-21 12:18:43 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+/*
+! let and const
+*/ 
+// let myname = "utkarsh";
+// myname = "This";
+// console.log(myname);
+//--------------------------
+
+/*
+! Arrow Function
+*/
+// let sayName = (x,Z) => console.log(`The sum of the number ${x} and ${Z} is = ${x+Z}`);
+
+// sayName(7,8);
+
+/*
+! Modules ES6
+*/
+import { hello,data as da } from "./sell";
+hello();
+console.log(da);
---
+++ customer.js
@@ -0,0 +1,5 @@
+export const person = {
+    name : "Utkarsh"
+}
+
+// export default person;
